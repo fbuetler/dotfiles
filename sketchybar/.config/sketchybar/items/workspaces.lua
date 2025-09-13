19821801ed9025771a2766e5cad075ad5a2cf495
Florian Bütler <florian.buetler@proton.me>
2025-09-13 14:15:10 +0200

    improve workspace lua script
    * no accordion when starting
    * no according when change monitor

--- sketchybar/.config/sketchybar/items/workspaces.lua
+++ sketchybar/.config/sketchybar/items/workspaces.lua
@@ -7,7 +7,7 @@ local QUERY_WORKSPACES =
 "aerospace list-workspaces --all --format '%{workspace}%{monitor-appkit-nsscreen-screens-id}' --json"
 
 local QUERY_VISIBLE_WORKSPACES =
-"aerospace list-workspaces --visible --monitor all --format '%{workspace}%{monitor-appkit-nsscreen-screens-id}' --json"
+"aerospace list-workspaces --visible --monitor all --format '%{workspace}' --json"
 
 local QUERY_FOCUSED_WORKSPACES = "aerospace list-workspaces --focused"
 
@@ -19,7 +19,6 @@ local APP_NAME_KEY = "app-name"
 
 local WORKSPACE_CHANGE_EVENT = "aerospace_workspace_change"
 local MONITOR_CHANGE_EVENT = "aerospace_display_change"
-local FOCUS_CHANGE_EVENT = "aerospace_focus_change"
 
 -- Add padding to the left
 local root = sbar.add("item", {
@@ -42,12 +41,22 @@ local function getWorkspacesState(f)
         sbar.exec(QUERY_FOCUSED_WORKSPACES, function(focused_workspaces)
             local focused_workspace = focused_workspaces:match("^%s*(.-)%s*$")
             sbar.exec(QUERY_VISIBLE_WORKSPACES, function(visible_workspaces)
-                local args = {
-                    open_windows = open_windows,
-                    focused_workspace = focused_workspace,
-                    visible_workspaces = visible_workspaces
-                }
-                f(args)
+                sbar.exec(QUERY_WORKSPACES, function(workspaces_and_monitors)
+                    local workspace_to_monitor = {}
+                    for _, entry in ipairs(workspaces_and_monitors) do
+                        local workspace_index = entry[WORKSPACE_KEY]
+                        local monitor_id = entry[MONITOR_KEY]
+                        workspace_to_monitor[workspace_index] = monitor_id
+                    end
+
+                    local args = {
+                        open_windows = open_windows,
+                        focused_workspace = focused_workspace,
+                        visible_workspaces = visible_workspaces,
+                        workspace_to_monitor = workspace_to_monitor
+                    }
+                    f(args)
+                end)
             end)
         end)
     end)
@@ -57,6 +66,7 @@ local function toggleWorkspaceVisibility(workspace_index, args)
     local open_windows = args.open_windows[workspace_index]
     local focused_workspace = args.focused_workspace
     local visible_workspaces = args.visible_workspaces
+    local workspace_to_monitor = args.workspace_to_monitor
 
     if open_windows == nil then
         open_windows = {}
@@ -67,17 +77,21 @@ local function toggleWorkspaceVisibility(workspace_index, args)
     for _, _ in ipairs(open_windows) do
         no_app = false
     end
+    local has_apps = not no_app
 
-    -- show empty focused workspace
+    local is_visible = false
     for _, visible_workspace in ipairs(visible_workspaces) do
-        if no_app and workspace_index == visible_workspace[WORKSPACE_KEY] then
-            local monitor_id = visible_workspace[MONITOR_KEY]
-
-            workspaces[workspace_index]:set({
-                display = monitor_id,
-            })
-            return
+        if workspace_index == visible_workspace[WORKSPACE_KEY] then
+            is_visible = true
+            break
         end
+    end
+
+    -- show empty focused workspace/non-empty workspace
+        workspaces[workspace_index]:set({
+            display = workspace_to_monitor[workspace_index],
+        })
+        return
     end
 
     -- hide empty unfocused workspace
@@ -93,23 +107,6 @@ local function toggleWorkspaceVisibilities()
     getWorkspacesState(function(args)
         for workspace_index, _ in pairs(workspaces) do
             toggleWorkspaceVisibility(workspace_index, args)
-        end
-    end)
-end
-
-local function displayWorkspacesOnMonitor()
-    local workspace_monitor = {}
-    sbar.exec(QUERY_WORKSPACES, function(workspaces_and_monitors)
-        for _, entry in ipairs(workspaces_and_monitors) do
-            local workspace_index = entry[WORKSPACE_KEY]
-            local monitor_id = entry[MONITOR_KEY]
-            workspace_monitor[workspace_index] = monitor_id
-        end
-
-        for workspace_index, _ in pairs(workspaces) do
-            workspaces[workspace_index]:set({
-                display = workspace_monitor[workspace_index],
-            })
         end
     end)
 end
@@ -138,6 +135,7 @@ sbar.exec(QUERY_WORKSPACES, function(workspaces_and_monitors)
                 border_color = colors.workspace.border,
                 border_width = settings.workspace.border.width,
             },
+            display = 0,
             click_script = "aerospace workspace " .. workspace_index,
         })
 
@@ -158,18 +156,21 @@ sbar.exec(QUERY_WORKSPACES, function(workspaces_and_monitors)
         end)
     end
 
+    -- all combinations of workspace and monitor change events may happen:
+    -- only workspace: change workspace on same monitor
+    -- only monitor: move workspace between monitors
+    -- both: change workspace on another monitor
+
     root:subscribe(WORKSPACE_CHANGE_EVENT, function()
         toggleWorkspaceVisibilities()
     end)
 
-    -- a monitor change also triggers a workspace change
     root:subscribe(MONITOR_CHANGE_EVENT, function()
-        displayWorkspacesOnMonitor()
+        toggleWorkspaceVisibilities()
     end)
 
-    -- initial setup
+    -- initial visibility
     toggleWorkspaceVisibilities()
-    displayWorkspacesOnMonitor()
 
     -- initial focus
     sbar.exec(QUERY_FOCUSED_WORKSPACES, function(focused_workspaces)
